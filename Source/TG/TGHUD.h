fd2bfa4a9e299a39aaf3a75b1bed6896f51ba43a
LAPTOP-BG4CG2U6\kmart <kmartin23>
2018-02-11 15:52:26 -0600

    Initial commit

---
+++ Source/TG/TGHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TGHUD.generated.h"
+
+UCLASS()
+class ATGHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ATGHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
